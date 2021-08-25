1f7b853d421ee83e6a1d12e68cb4e4ab12075f41
Husan Sattarov <sattarovhusan@gmail.com>
2021-08-25 19:11:31 -0400

    First commit

---
+++ routes/index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router  = express.Router();
+const {ensureAuthenticated} = require('../config/auth') 
+const Project = require("../models/project");
+const User = require("../models/user");
+
+//login page
+router.get('/', (req,res)=>{
+    res.render('login');
+})
+//register page
+router.get('/register', (req,res)=>{
+    res.render('register');
+})
+router.get('/dashboard',ensureAuthenticated,(req,res)=>{
+    User.find({}, (err, users) => { 
+        req.users = users;
+    }); 
+    Project.find({'user_id' : req.user._id}).exec((err,projects)=>{
+        res.render('dashboard',{
+            user: req.user,
+            projects: projects,
+            users: req.users
+        });  
+    })
+})
+
+// delete
+router.get('/dashboard/:id',(req,res)=>{
+    Project.findByIdAndRemove(req.params.id, function(err){
+        res.redirect("/dashboard");
+     });
+})
+
+
+module.exports = router; 
